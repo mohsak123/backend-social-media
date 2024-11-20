abda6937786b405516cfe47034fa690beb2c7533
mohsak123 <143778086+mohsak123@users.noreply.github.com>
2024-11-20 11:23:29 +0300

    Update sendEmail.js v2

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,32 +4,42 @@ let transporter = nodemailer.createTransport({
   secure: true,
   service: "gmail",
   host: "smtp.gmail.com",
-  // host: "smtp.gmail.com",
   port: 465,
   auth: {
-    user: "kogoshakokah@gmail.com",
-    pass: "mwlejmunxkamlwqe",
+    user: "kogoshakokah@gmail.com", // Your Gmail address
+    pass: "mwlejmunxkamlwqe", // App password
   },
 });
 
+// Verify the transporter configuration
 transporter.verify((error, success) => {
   if (error) {
-    console.log(error);
+    console.log("Error:", error);
   } else {
-    console.log("Ready for messages");
+    console.log("Ready to send emails");
   }
 });
 
+// Function to send emails
 function sendMail(to, sub, msg) {
-  transporter.sendMail({
-    to: to,
-    subject: sub,
-    html: msg,
-  });
-
-  console.log("send email");
+  transporter.sendMail(
+    {
+      from: "kogoshakokah@gmail.com", // Sender email
+      to: to,
+      subject: sub,
+      html: msg, // HTML content of the email
+    },
+    (err, info) => {
+      if (err) {
+        console.error("Error sending email:", err);
+      } else {
+        console.log("Email sent successfully:", info.response);
+      }
+    }
+  );
 }
 
-// sendMail("kogoshakokah@gmail.com", "This is subject", "this is message");
+// Uncomment the line below to test the function
+// sendMail("kogoshakokah@gmail.com", "This is subject", "This is message");
 
 module.exports = { sendMail };
